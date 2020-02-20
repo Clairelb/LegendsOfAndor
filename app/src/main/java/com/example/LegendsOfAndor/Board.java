0cd34c51272c5a25c76c44e2273cedf8d2197d0b
xiaobin <xiaobin.shang@mail.mcgill.ca>
2020-02-19 19:10:32 -0500

    Check the adjacentRegion.

--- app/src/main/java/com/example/LegendsOfAndor/Board.java
+++ app/src/main/java/com/example/LegendsOfAndor/Board.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -62,14 +63,29 @@ public class Board extends AppCompatActivity {
         if(flag){
             this.flag = false;
             Bitmap layout = BitmapFactory.decodeResource(getResources(),R.drawable.overlay);
-            int color = layout.getPixel((int)(event.getX()),(int)(event.getY()));
-            int regionNumber = Color.blue(color);
+
+            int newColor = layout.getPixel((int)(event.getX()),(int)(event.getY()));
+            int newRegionNumber = Color.blue(newColor);
+            Region newRegion = RegionDatabase.getInstance().getRegionDatabase().get(newRegionNumber);
+
+            int oldColor = layout.getPixel((int)(this.warrior.getX()),(int)(this.warrior.getY()));
+            int oldRegionNumber = Color.blue(oldColor);
+            Region currentRegion = RegionDatabase.getInstance().getRegionDatabase().get(oldRegionNumber);
+
+            if(currentRegion.getAdjacentRegions().contains(newRegion))
+            {
+                this.warrior.setX(event.getX());
+                this.warrior.setY(event.getY());
+                return true;
+            }
+            else
+                {
+                    Toast.makeText(Board.this, "The clicked region is not a neighbor to your current region. Please choose your destination again.", Toast.LENGTH_LONG).show();
+                    return super.dispatchTouchEvent(event);
+                }
             //check if the region is a neighbor to the current region;
             //set the player's current region to new region;
-            //add the player to the new region and remove the player from the previous region;
-            this.warrior.setX(event.getX());
-            this.warrior.setY(event.getY());
-            return true;
+            //add the player to the new region and remove the player from the previous region
         }
         return super.dispatchTouchEvent(event);
     }
