c6eaad5728e3fe735ea0d5f92fa631074908e3eb
Bowen <bowenlig@gmail.com>
2020-03-23 19:32:08 -0400

    small changes

--- app/src/main/java/com/example/LegendsOfAndor/LegendCardG.java
+++ app/src/main/java/com/example/LegendsOfAndor/LegendCardG.java
@@ -2,13 +2,61 @@ package com.example.LegendsOfAndor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class LegendCardG extends AppCompatActivity {
+    private Button toBoardBTN;
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_legend_card_g);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        toBoardBTN =findViewById(R.id.to_board_g);
+        final MyPlayer myPlayer = MyPlayer.getInstance();
+
+        toBoardBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myPlayer.getPlayer().getUsername().equals(myPlayer.getGame().getPlayers()[0].getUsername())) {
+                    ActivateLegendCardGSender activateLegendCardGSender = new ActivateLegendCardGSender();
+                    activateLegendCardGSender.execute("");
+                }
+
+                Intent intent = new Intent(LegendCardG.this, Board.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {}
+
+    private static class ActivateLegendCardGSender extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... strings) {
+            MyPlayer myPlayer = MyPlayer.getInstance();
+
+            try {
+                Unirest.post("http://"+myPlayer.getServerIP()+":8080/"+myPlayer.getGame().getGameName() +"/"+ myPlayer.getPlayer().getUsername() + "/activateLegendCardG")
+                        .asString();
+
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
