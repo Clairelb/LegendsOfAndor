3a07a6d1d7e63f7bccd5c767013e854ec70e794b
Bowen <bowenlig@gmail.com>
2020-03-22 20:50:23 -0400

    added all new legend card activities

---
+++ app/src/main/java/com/example/LegendsOfAndor/legend_card_rune_stones_hard.java
@@ -0,0 +1,14 @@
+package com.example.LegendsOfAndor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class legend_card_rune_stones_hard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legend_card_rune_stones_hard);
+    }
+}
